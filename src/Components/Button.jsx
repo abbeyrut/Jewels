ba2ae3ad6ec88fd86d1f81c4e5d899fa991ae239
Abbey Rut <abbey.rut@gmail.com>
2022-12-08 20:16:14 +0200

    button name

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -6,7 +6,7 @@ const Button =  ({title,onClick}) => {
     <div className="main col-1">
         <p><h2> {title}</h2></p>
         
-        <div><p><button className="btn" onClick={onClick}> jewel 1</button></p>
+        <div><p><button className="btn" onClick={onClick}> jewel 231</button></p>
        <p><button className="btn" onClick={onClick}> jewel 2</button></p> 
        <p><button className="btn" onClick={onClick}> jewel 3</button></p> 
        </div>
